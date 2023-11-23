f3e5ded540bedf5916d8a475b50d669b09f49bd2
FaithE160 <faith.egan16@gmail.com>
2023-11-23 14:52:21 +0000

    Changed Game Name

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 # Title of the game
-root.title("Bounce")
+root.title("Bounce Breaker 2.0")
 root.geometry("500x670")
 root.resizable(0, 0)
 root.wm_attributes("-topmost", 1)
