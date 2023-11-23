bb0f730a461040c761e153a8f038fff1db84dc3a
FaithE160 <faith.egan16@gmail.com>
2023-11-23 16:25:23 +0000

    Scoring changed, Ball colour changed, background changed, message changed

--- game.py
+++ game.py
@@ -23,13 +23,13 @@ def start_game(event):
         file1.close()
         score.configure(text="Score: 00\n\nHighscore: " + highscore)
         canvas.delete("all")
-        BALL_COLOR = ["red", "yellow", "green"]
+        BALL_COLOR = ["pink", "purple", "cyan"]
         BRICK_COLOR = ["PeachPuff3", "dark slate gray", "rosy brown", "light goldenrod yellow", "turquoise3", "salmon",
                        "light steel blue", "dark khaki", "pale violet red", "orchid", "tan", "MistyRose2",
                        "DodgerBlue4", "wheat2", "RosyBrown2", "bisque3", "DarkSeaGreen1"]
         random.shuffle(BALL_COLOR)
         # Colour of the paddle
-        paddle = Paddle(canvas, "blue")
+        paddle = Paddle(canvas, "yellow")
         bricks = []
         for i in range(0, 5):
             b = []
@@ -63,7 +63,7 @@ def start_game(event):
                     time.sleep(0.01)
                     if ball.hit==95:
                         # You won message and colour
-                        canvas.create_text(250, 250, text="YOU WON !!", fill="yellow", font="Consolas 24 ")
+                        canvas.create_text(250, 250, text="YOU WON, HURRAY !!", fill="yellow", font="Consolas 24 ")
                         root.update_idletasks()
                         root.update()
                         playing = False
@@ -77,7 +77,7 @@ def start_game(event):
                         file1 = open("highscore.txt","w")
                         file1.write(str(ball.hit))
                         file1.close()
-                    canvas.create_text(250, 250, text="GAME OVER!!\nYour score was: " + str(ball.hit), fill="red", font="Consolas 24 ")
+                    canvas.create_text(250, 250, text="GAME OVER, WOMP WOMP!!\nYour score was: " + str(ball.hit), fill="red", font="Consolas 24 ")
                     root.update_idletasks()
                     root.update()
                     playing = False
